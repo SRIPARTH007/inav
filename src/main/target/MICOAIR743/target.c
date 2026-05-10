191c88f1f461c626de3ead72f4970a940fed70e5
SRIPARTH007 <143305876+SRIPARTH007@users.noreply.github.com>
2026-05-10 10:21:11 +0530

    Update target.c

--- src/main/target/MICOAIR743/target.c
+++ src/main/target/MICOAIR743/target.c
@@ -1,42 +1,61 @@
-/*
-* This file is part of Cleanflight.
-*
-* Cleanflight is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* Cleanflight is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with Cleanflight.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include <stdint.h>
-
 #include "platform.h"
-
 #include "drivers/bus.h"
 #include "drivers/io.h"
 #include "drivers/pwm_mapping.h"
 #include "drivers/timer.h"
-#include "drivers/pinio.h"
-#include "drivers/sensor.h"
+#include "drivers/system_stm32h7xx.h"
 
+// Timer Hardware Map from Schematic Page 4/4
 timerHardware_t timerHardware[] = {
-    DEF_TIM(TIM1,  CH4, PE14,  TIM_USE_OUTPUT_AUTO, 0, 0), // S1
-    DEF_TIM(TIM1,  CH3, PE13,  TIM_USE_OUTPUT_AUTO, 0, 1), // S2
-    DEF_TIM(TIM1,  CH2, PE11,  TIM_USE_OUTPUT_AUTO, 0, 2), // S3
-    DEF_TIM(TIM1,  CH1, PE9,   TIM_USE_OUTPUT_AUTO, 0, 3), // S4
-    DEF_TIM(TIM3,  CH4, PB1,   TIM_USE_OUTPUT_AUTO, 0, 4), // S5
-    DEF_TIM(TIM3,  CH3, PB0,   TIM_USE_OUTPUT_AUTO, 0, 5), // S6
-    DEF_TIM(TIM4,  CH1, PD12,  TIM_USE_OUTPUT_AUTO, 0, 6), // S7
-    DEF_TIM(TIM4,  CH2, PD13,  TIM_USE_OUTPUT_AUTO, 0, 7), // S8
-    DEF_TIM(TIM4,  CH3, PD14,  TIM_USE_OUTPUT_AUTO, 0, 0), // S9
-    DEF_TIM(TIM4,  CH4, PD15,  TIM_USE_OUTPUT_AUTO, 0, 0), // S10 DMA_NONE
+    DEF_TIM(TIM8,  CH1, PC6,  TIM_USE_OUTPUT_AUTO, 0, 0), // Motor 1
+    DEF_TIM(TIM8,  CH2, PC7,  TIM_USE_OUTPUT_AUTO, 0, 1), // Motor 2
+    DEF_TIM(TIM8,  CH3, PC8,  TIM_USE_OUTPUT_AUTO, 0, 2), // Motor 3
+    DEF_TIM(TIM8,  CH4, PC9,  TIM_USE_OUTPUT_AUTO, 0, 3), // Motor 4
+    DEF_TIM(TIM1,  CH1, PE9,  TIM_USE_OUTPUT_AUTO, 0, 4), // Servo 1
+    DEF_TIM(TIM1,  CH2, PE11, TIM_USE_OUTPUT_AUTO, 0, 5), // Servo 2
+    DEF_TIM(TIM1,  CH3, PE13, TIM_USE_OUTPUT_AUTO, 0, 6), // Servo 3
+    DEF_TIM(TIM1,  CH4, PE14, TIM_USE_OUTPUT_AUTO, 0, 7), // Servo 4
 };
 
 const int timerHardwareCount = sizeof(timerHardware) / sizeof(timerHardware[0]);
+
+// OVERRIDE: 48MHz Clock Configuration
+void targetSystemClockConfig(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
+    while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    
+    // Divide 48MHz by 48 to get the 1MHz PLL input
+    RCC_OscInitStruct.PLL.PLLM = 48; 
+    
+    RCC_OscInitStruct.PLL.PLLN = 480;
+    RCC_OscInitStruct.PLL.PLLP = 2;   // CPU at 480MHz
+    RCC_OscInitStruct.PLL.PLLQ = 20;  // USB at 48MHz
+    RCC_OscInitStruct.PLL.PLLR = 2;
+    RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+    RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_0;
+    
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
+                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.AHBDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB3Divider = RCC_APB3_DIV2;
+    RCC_ClkInitStruct.APB1Divider = RCC_APB1_DIV2;
+    RCC_ClkInitStruct.APB2Divider = RCC_APB2_DIV2;
+    RCC_ClkInitStruct.APB4Divider = RCC_APB4_DIV2;
+
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
+}
