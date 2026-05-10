efdaa6c30ff4bf21955628a698388afca0eb492f
SRIPARTH007 <143305876+SRIPARTH007@users.noreply.github.com>
2026-05-10 16:48:03 +0530

    Update target.h

--- src/main/target/MICOAIR743/target.h
+++ src/main/target/MICOAIR743/target.h
@@ -1,7 +1,6 @@
 #pragma once
 
 // *************** GPIO PORT UNLOCKS **********************
-// Necessary to prevent "No pins are defined" and "Not supported" errors
 #define TARGET_IO_PORTA 0xFFFF
 #define TARGET_IO_PORTB 0xFFFF
 #define TARGET_IO_PORTC 0xFFFF
@@ -12,62 +11,8 @@
 #define TARGET_BOARD_IDENTIFIER "M743"
 #define USBD_PRODUCT_STRING     "KSPR_H743_V4"
 
-// *************** 48MHz CLOCK CONFIGURATION **********************
-#ifdef HSE_VALUE
-#undef HSE_VALUE
-#endif
-#define HSE_VALUE       48000000
-
-// *************** LED **********************
-#define LED0                    PB11
-#define LED1                    PE10
-#define LED2                    PE15
-
-// *************** SPI DEVICES **********************
-#define USE_SPI
-#define USE_SPI_DEVICE_2
-#define USE_SPI_DEVICE_6
-
-// Primary IMU: ICM42688 on SPI6
-#define SPI6_SCK_PIN            PA5
-#define SPI6_MISO_PIN           PA6
-#define SPI6_MOSI_PIN           PA7
-#define USE_IMU_ICM42688
-#define ICM42688_SPI_BUS        BUS_SPI6
-#define ICM42688_CS_PIN         PA15
-
-// Secondary IMU: BMI088 on SPI2
-#define SPI2_SCK_PIN            PB13
-#define SPI2_MISO_PIN           PB14
-#define SPI2_MOSI_PIN           PB15
-#define USE_IMU_BMI088
-#define BMI088_SPI_BUS          BUS_SPI2
-#define BMI088_GYRO_CS_PIN      PB12
-#define BMI088_ACC_CS_PIN       PB0
-
-// *************** I2C DEVICES **********************
-#define USE_I2C
-#define USE_I2C_DEVICE_1
-#define USE_I2C_DEVICE_4
-
-// I2C1 (Magnetometer) on PB8, PB9
-#define I2C1_SCL                PB8
-#define I2C1_SDA                PB9
-
-// I2C4 (Barometer) on PB6, PB7
-#define I2C4_SCL                PB6
-#define I2C4_SDA                PB7
-
-#define USE_BARO
-#define USE_BARO_BMP388
-#define BARO_I2C_BUS            BUS_I2C4
-
-#define USE_MAG
-#define USE_MAG_BMM350
-#define MAG_I2C_BUS             BUS_I2C1
-
 // *************** SERIAL / UART ********************
-// These fix the "SERIAL_PORT_COUNT undeclared" error
+// MOVED UP to ensure visibility in config.c/serial.h
 #define USE_UART1
 #define UART1_TX_PIN            PA9
 #define UART1_RX_PIN            PA10
@@ -86,9 +31,58 @@
 
 #define SERIAL_PORT_COUNT       4
 
+// *************** 48MHz CLOCK CONFIGURATION **********************
+#ifdef HSE_VALUE
+#undef HSE_VALUE
+#endif
+#define HSE_VALUE       48000000
+
+// *************** LED **********************
+#define LED0                    PB11
+#define LED1                    PE10
+#define LED2                    PE15
+
+// *************** SPI DEVICES **********************
+#define USE_SPI
+#define USE_SPI_DEVICE_2
+#define USE_SPI_DEVICE_6
+
+#define SPI6_SCK_PIN            PA5
+#define SPI6_MISO_PIN           PA6
+#define SPI6_MOSI_PIN           PA7
+#define USE_IMU_ICM42688
+#define ICM42688_SPI_BUS        BUS_SPI6
+#define ICM42688_CS_PIN         PA15
+
+#define SPI2_SCK_PIN            PB13
+#define SPI2_MISO_PIN           PB14
+#define SPI2_MOSI_PIN           PB15
+#define USE_IMU_BMI088
+#define BMI088_SPI_BUS          BUS_SPI2
+#define BMI088_GYRO_CS_PIN      PB12
+#define BMI088_ACC_CS_PIN       PB0
+
+// *************** I2C DEVICES **********************
+#define USE_I2C
+#define USE_I2C_DEVICE_1
+#define USE_I2C_DEVICE_4
+
+#define I2C1_SCL                PB8
+#define I2C1_SDA                PB9
+#define I2C4_SCL                PB6
+#define I2C4_SDA                PB7
+
+#define USE_BARO
+#define USE_BARO_BMP388
+#define BARO_I2C_BUS            BUS_I2C4
+
+#define USE_MAG
+#define USE_MAG_BMM350
+#define MAG_I2C_BUS             BUS_I2C1
+
 // *************** ADC *****************************
 #define USE_ADC
 #define VBAT_ADC_PIN            PC0 
 #define CURRENT_METER_ADC_PIN   PC1
 
-// Force build start 7
+// Force build start 8
