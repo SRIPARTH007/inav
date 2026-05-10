50c157dfcefafb2f0ed36dad3dc37b6662cc377c
SRIPARTH007 <143305876+SRIPARTH007@users.noreply.github.com>
2026-05-10 11:45:14 +0530

    target.h

--- src/main/target/MICOAIR743/target.h
+++ src/main/target/MICOAIR743/target.h
@@ -3,15 +3,14 @@
 #define TARGET_BOARD_IDENTIFIER "M743"
 #define USBD_PRODUCT_STRING     "KSPR_H743_V4"
 
-// *************** 48MHz CLOCK CONFIGURATION  **********************
-// Essential: Schematic shows 48.000MHz Crystal
+// *************** 48MHz CLOCK CONFIGURATION **********************
+// Fixed: Using undef/def to prevent "redefined" error
 #ifdef HSE_VALUE
 #undef HSE_VALUE
 #endif
 #define HSE_VALUE       48000000
 
 // *************** LED **********************
-// Mapped to your pins: PB11 (LED3), PE10 (LED1), PE15 (LED2)
 #define LED0                    PB11  
 #define LED1                    PE10  
 #define LED2                    PE15  
@@ -21,7 +20,7 @@
 #define USE_SPI_DEVICE_2
 #define USE_SPI_DEVICE_6
 
-// Primary IMU: ICM42688 on SPI6 (PA5, PA6, PA7)
+// Primary IMU: ICM42688 on SPI6
 #define SPI6_SCK_PIN            PA5
 #define SPI6_MISO_PIN           PA6
 #define SPI6_MOSI_PIN           PA7
@@ -29,7 +28,7 @@
 #define ICM42688_SPI_BUS        BUS_SPI6
 #define ICM42688_CS_PIN         PA15
 
-// Secondary IMU: BMI088 on SPI2 (PB13, PB14, PB15)
+// Secondary IMU: BMI088 on SPI2
 #define SPI2_SCK_PIN            PB13
 #define SPI2_MISO_PIN           PB14
 #define SPI2_MOSI_PIN           PB15
@@ -39,17 +38,18 @@
 #define BMI088_ACC_CS_PIN       PB0
 
 // *************** I2C DEVICES **********************
+// Fixed: Wrapped pins in IO_TAG to prevent macro expansion errors
 #define USE_I2C
 #define USE_I2C_DEVICE_1
 #define USE_I2C_DEVICE_4
 
 // Baros on I2C4 (PB6, PB7)
-#define I2C4_SCL                PB6
-#define I2C4_SDA                PB7
+#define I2C4_SCL                IO_TAG(PB6)
+#define I2C4_SDA                IO_TAG(PB7)
 
 // Magnetometer on I2C1 (PB8, PB9)
-#define I2C1_SCL                PB8
-#define I2C1_SDA                PB9
+#define I2C1_SCL                IO_TAG(PB8)
+#define I2C1_SDA                IO_TAG(PB9)
 
 #define USE_BARO
 #define USE_BARO_BMP388
